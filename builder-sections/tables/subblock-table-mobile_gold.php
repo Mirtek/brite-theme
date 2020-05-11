b5bb39c7a809543c70c2c74d1c50d74f491860aa
Denys <gg.zyus@gmail.com>
2020-05-11 08:03:28 -0400

    camps done

---
+++ builder-sections/tables/subblock-table-mobile_gold.php
@@ -0,0 +1,85 @@
+<?php 
+      $table = get_sub_field( 'pricing_table_mobile_gold' );
+      if ( ! empty ( $table ) ) {
+          $first_header_flag = true;
+
+          echo '<table class="table-plans table-plans_mobile table-plans_gold">';
+
+              echo '<tbody>';
+              if ( ! empty( $table['header'] ) ) {
+
+
+                      echo '<tr>';
+
+                          foreach ( $table['header'] as $th ) {
+                            if ($first_header_flag) {
+                              echo '<th colspan=3>';
+                                  echo $th['c'];
+                              echo '</th>';
+                              $first_header_flag=false;
+                            }
+                            else {
+                              if ($th['c']) {                              
+                                echo '<th>';
+                                    echo $th['c'];
+                                echo '</th>';
+                              }
+                            }
+
+
+                          }
+
+                      echo '</tr>';
+
+              }
+
+                  $first_in_table=true;
+                  $second_in_row=true;
+                  $third_in_row=true;
+
+                  foreach ( $table['body'] as $tr ) {
+                      $first_in_row=true;
+
+                          foreach ( $tr as $td ) {
+
+                              if ($td['c']) {
+                                # code...
+                                if ($first_in_table) {
+                                  echo '<th colspan=3>';
+                                      echo $td['c'];
+                                  echo '</th>';
+                                  $first_in_table=false;
+                                } elseif ($first_in_row) {
+                                  echo '<th>';
+                                      echo $td['c'];
+                                  echo '</th>';
+                                  $first_in_row=false;
+                                  # code...
+                                } elseif ($second_in_row) {
+                                  echo '<th>';
+                                      echo $td['c'];
+                                  echo '</th>';
+                                  $second_in_row = false;
+                                } elseif ($third_in_row) {
+                                  echo '<th>';
+                                      echo $td['c'];
+                                  echo '</th>';
+                                  $third_in_row = false;
+                                } else {
+                                  echo '<td>';
+                                      echo $td['c'];
+                                  echo '</td>';                                        
+                                }
+//                                $first_in_row=false;
+                              }
+
+                        }
+
+                      echo '</tr>';
+                  }
+
+              echo '</tbody>';
+
+          echo '</table>';
+      }
+?>
---
+++ builder-sections/tables/subblock-table_silver.php
@@ -0,0 +1,63 @@
+<?php 
+      $table = get_sub_field( 'pricing_table' );
+      if ( ! empty ( $table ) ) {
+          $first_header_flag = true;
+
+          echo '<table class="table-plans table-plans_desktop table-plans_silver">';
+
+              echo '<tbody>';
+              if ( ! empty( $table['header'] ) ) {
+
+
+                      echo '<tr>';
+
+                          foreach ( $table['header'] as $th ) {
+                            if ($first_header_flag) {
+                              echo '<th rowspan=3>';
+                                  echo $th['c'];
+                              echo '</th>';
+                              $first_header_flag=false;
+                            }
+                            else {
+                              echo '<th>';
+                                  echo $th['c'];
+                              echo '</th>';
+                            }
+
+
+                          }
+
+                      echo '</tr>';
+
+              }
+
+                  foreach ( $table['body'] as $tr ) {
+                    $first_in_row=true;
+                      echo '<tr>';
+
+                          foreach ( $tr as $td ) {
+                              if ($td['c']) {
+                                # code...
+                                if ($first_in_row) {
+                                  echo '<th>';
+                                      echo $td['c'];
+                                  echo '</th>';
+
+                                } else {
+                                  echo '<td>';
+                                      echo $td['c'];
+                                  echo '</td>';                                        
+                                }
+                                $first_in_row=false;
+                              }
+
+                          }
+
+                      echo '</tr>';
+                  }
+
+              echo '</tbody>';
+
+          echo '</table>';
+      }
+?>
